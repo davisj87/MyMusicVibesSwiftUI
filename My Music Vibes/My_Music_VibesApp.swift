c55b9077191210e256a81c8cbb58d32decfac732
Jarred <Jarred.davis87@gmail.com>
2023-03-07 10:18:55 -0500

    Initial Commit

---
+++ My Music Vibes/My_Music_VibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Music_VibesApp.swift
+//  My Music Vibes
+//
+//  Created by Jarred Davis on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct My_Music_VibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
